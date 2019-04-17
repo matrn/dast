2ee52716edf5660ecdb4de3adfd5312239da3fb2
matrn <trnka.matej@seznam.cz>
2019-04-17 17:42:16 +0200

    improving helpers functions

--- C/helpers.c
+++ C/helpers.c
@@ -21,54 +21,36 @@ s_byte dast_parse_time(char delimiter, char * input, long * time, char ** data){
 	  0 = success
 	*/
 
-	//char format[10];
 	char * time_str;
-	//char delim_str[2];
+
 	unsigned int input_len = 0;
 	unsigned int delim_pos = 0;
 
-	/*
-	delim_str[0] = delimiter;
-	delim_str[1] = 0;
-	
-	//time_str = malloc(50);
-	// *data = malloc(60);
-	printf("Input >%s<\n", input);
-	char * input2 = malloc(50);
-	strcpy(input2, "5454-test");
-	printf("pos %d\n", get_pos("fsdfsdfttt", '-'));
 
-	return 1;
-	*/
 	input_len = strlen(input);
 	
-	if(in_str(input, delimiter)){
+	if(in_str(input, delimiter)){   /* check delimiter in input string */
 		delim_pos = get_pos(input, delimiter);   /* get position of delimiter, we can use unsigned int because if string contains delimiter was already checked by in_str() function */
 
+		/* allocate memory for data and copy them */
 		*data = malloc(input_len - delim_pos + 1);
 		strcpy(*data, input + delim_pos + 1);
+
 
 		if(delim_pos == 0) return -2;   /* return that delimiter is here but time not */
 
 
+		/* allocate memory for time string and copy them into this string */
 		time_str = malloc(delim_pos + 1);   /* allocate memory for time, +1 is for ending 0 */
 		strncpy(time_str, input, delim_pos);   /* copy time without data */
 		time_str[delim_pos] = 0;   /* end of str */
 
-		/*
-		printf("time str >%s<\n", time_str);
-		printf("strlen: %d, pos: %d\n", input_len, delim_pos);
-		*/
-		// *data[input_len - pos] = 0;
-
 		errno = 0;	/* to distinguish success/failure after call */
 		*time = strtol(time_str, NULL, 10);
-		free(time_str);
 
-		//if(time_str) 
-		//else return -2;   /* no time is in input string */
+		free(time_str);   /* free time string variable */
 
-		
+
 		/* check for various possible errors during strtol() */
 		if ((errno == ERANGE && (*time == LONG_MAX || *time == LONG_MIN)) || (errno != 0 && *time == 0)) {
 			//perror("strtol");
@@ -76,13 +58,6 @@ s_byte dast_parse_time(char delimiter, char * input, long * time, char ** data){
 			return -3;   /* strtol error */
 		}
 
-		
-		/*
-		sprintf(format, "%%lu%c%%ms", delimiter);
-		printf("input >%s<\n", input);
-		printf("sscanf: %d\n", sscanf(input, "%lu-%ms", &*time, &*data));
-		*/
-		//printf("Time >%s< data >%s<\n", time_str, *data);
 		return 0;   /* success */
 	}
 	else{
